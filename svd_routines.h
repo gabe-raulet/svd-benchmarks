930a610d431c05a3335eda61b5a4ad65ecdb4a9a
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-11-20 20:15:16 -0800

    generalize

--- svd_routines.h
+++ svd_routines.h
@@ -3,63 +3,42 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <math.h>
 #include "lapacke.h"
 
 #define min(a, b) (((a) < (b))? (a) : (b))
 #define max(a, b) (((a) > (b))? (a) : (b))
 
-
-    /*
-     * DGESVD computes the SVD of a real m-by-n matrix A = U * SIGMA * transpose(V).
-     *
-     * jobu:
-     *      'A': all m columns of U are returned in array U
-     *      'S': the first min(m,n) columns of U (the left singular vectors) are returned in the array U
-     *      'N': no columns of U are computed
-     *
-     *  jobvt:
-     *      'A': all n rows of V^T are returned in the array vt.
-     *      'S': the first min(m,n) rows ov V^T (the right singular vectors) are returned in the array VT.
-     *      'N': no rows of V&T are computed.
-     *
-     *  m: number of rows of the input matrix A.
-     *
-     *  n: number of columns of the input matrix A.
-     *
-     *  A: double precision array, dimension (lda,n). On entry, the m-by-n matrix.
-     *
-     *  lda: the leading dimension of the array A. lda >= max(1,m).
-     *
-     *  s: double precision array, dimension (min(m,n)). The singular vales of A, sorted so that s(i) >= s(i+1).
-     *
-     *  u: double precision array, dimension (ldu,m) if jobu='A' or (ldu,min(m,n)) if jobu='S'.
-     *     If jobu='A', U contains the m-by-m orthogonal matrix U;
-     *     If jobu='S', U contains the first min(m,n) columns of U (the left singular vectors, stored columnwise);
-     *
-     *  ldu: The leading dimension of the array U. ldu >= 1; if jobu='S' or jobu='A', ldu >= m.
-     *
-     *  vt: double precision array, dimension (ldvt,n).
-     *      If jobvt = 'A', vt contains the n-by-n orthogonal matrix V^T.
-     *      if jobvt = 'S', vt contains the first min(m,n) rows of V^T (the right singular vectors, stored rowwise).
-     *
-     *  ldvt: the leading dimension of the array vt. ldvt >= 1; if jobvt = 'A', ldvt >= n; if jobvt = 'S', ldvt >= min(m,n).
-     *
-     *  work: work is double precision array,dimension (max(1,lwork))
-     *
-     *  lwork: dimension of the array work. lwork >= max(1,5*min(m,n)).
-     *
-     */
-
-int serial_thin_lapack_svd_values(double *A, double *svals, size_t m, size_t n)
+int serial_thin_lapack_svd(double *A, double **SIGMA_ref, double **U_ref, double **Vt_ref, int m, int n)
 {
-    int lda = m;
-    int ldu = 1;
-    int ldvt = 1;
-    int lwork = 5 * min(m,n);
-    double *work = malloc(lwork * sizeof(double));
+    assert(A != NULL && SIGMA_ref != NULL && m >= 1 && n >= 1);
 
-    LAPACKE_dgesvd(LAPACK_COL_MAJOR, 'N', 'N', m, n, A, lda, svals, NULL, ldu, NULL, ldvt, work);
+    int drank = min(m, n); /* minimum dimension */
+    int lda = m; /* leading dimension of array A */
+    int ldu = U_ref? m : 1; /* leading dimension of array U (1 if not calculated) */
+    int ldvt = Vt_ref? drank : 1; /* leading dimension of array Vt (1 if not calculated) */
+    int lwork = 5 * drank; /* workspace array length */
+    double *work = malloc(lwork*sizeof(double)); /* workspace */
+    double *SIGMA = malloc(drank*sizeof(double)); /* singular values array */
+    double *U = U_ref? malloc(ldu*drank*sizeof(double)) : NULL; /* left singular vectors array */
+    double *Vt = Vt_ref? malloc(ldvt*n*sizeof(double)) : NULL; /* right singular vectors array */
+    char jobu = U_ref? 'S' : 'N';
+    char jobvt = Vt_ref? 'S' : 'N';
+
+    assert(work != NULL);
+    assert(SIGMA != NULL);
+    if (U_ref) assert(U != NULL);
+    if (Vt_ref) assert(Vt != NULL);
+
+    int info = LAPACKE_dgesvd(LAPACK_COL_MAJOR, jobu, jobvt, m, n, A, lda, SIGMA, U, ldu, Vt, ldvt, work);
+
+    assert(info == 0);
+
+    *SIGMA_ref = SIGMA;
+
+    if (U_ref) *U_ref = U;
+    if (Vt_ref) *Vt_ref = Vt;
 
     free(work);
 
--- test.c
+++ test.c
@@ -11,33 +11,32 @@ int main(int argc, char *argv[])
 {
 
     MM_typecode matcode;
-    int mi, ni;
+    int m, n;
     FILE *fp = fopen("A.mtx", "r");
 
     mm_read_banner(fp, &matcode);
     assert(mm_is_dense(matcode));
 
-    mm_read_mtx_array_size(fp, &mi, &ni);
+    mm_read_mtx_array_size(fp, &m, &n);
 
-    double *A = malloc(mi*ni*sizeof(double));
+    double *A = malloc(m*n*sizeof(double));
 
     /*
      * REMEMBER: matrix market stores dense matrices in COLUMN MAJOR order.
      */
-    for (int i = 0; i < mi*ni; ++i)
+    for (int i = 0; i < m*n; ++i)
         fscanf(fp, "%lg", &A[i]);
 
     fclose(fp);
 
-    int p = mi < ni? mi : ni;
-    double *svals = malloc(p * sizeof(double));
+    double *SIGMA, *U, *Vt;
 
-    serial_thin_lapack_svd_values(A, svals, (size_t)mi, (size_t)ni);
+    serial_thin_lapack_svd(A, &SIGMA, &U, &Vt, m, n);
 
-
+    int p = m < n? m : n;
     for (int i = 0; i < p; ++i)
     {
-        printf("%.18e\n", svals[i]);
+        printf("%.18e\n", SIGMA[i]);
     }
 
     return 0;
